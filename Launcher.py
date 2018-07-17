4b18f1c66506aa4ebea4a14f9132a8e6500931e4
Fan.Hu <fan.hu@t2mobile.com>
2018-07-17 17:02:51 +0800

    set min duration for checking version

--- Launcher.py
+++ Launcher.py
@@ -4,10 +4,14 @@
 from cmd.base.Cmd import Cmd
 from script.util.Print import Print
 
-import sys
+import time
 import os
 from urllib.request import Request
 from urllib.request import urlopen
+
+_PREFIX_CMD = 'cmd:'
+_PREFIX_FUNC = 'func:'
+UPDATE_DURATION = 3600
 
 # User Agent
 UA_CHROME = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0)' \
@@ -21,7 +25,57 @@ URL_VERSION = 'https://raw.githubusercontent.com/vancebs/EasyCoding3/master/VERS
 LOCAL_VERSION = '%s/VERSION' % os.path.dirname(__file__)
 
 
+def shell_exec(cmd: str) -> int:
+    # send command
+    print('%s%s' % (_PREFIX_CMD, cmd))
+
+    # receive exit code
+    line = input()
+
+    # get exit code
+    try:
+        return int(line)
+    except ValueError:
+        return 10086  # unknown error
+
+
+def shell_func(cmd: str) -> str:
+    # send command
+    print('%s%s' % (_PREFIX_FUNC, cmd))
+
+    # receive output
+    output = []
+    line = input().strip()
+    while not line == '==end==':
+        output.append(line)
+        line = input().strip()
+
+    # get result
+    return '\n'.join(output).strip()
+
+
 def launch():
+    # get last update time
+    str_last_update_time = shell_func('eval echo ${EC3_LAST_UPDATE_TIME}').strip()
+    last_update_time = 0
+    if str_last_update_time != '':
+        last_update_time = int(str_last_update_time)
+
+    # get current time
+    curr_update_time = int(time.time())
+
+    # check update duration
+    delta_time = curr_update_time - last_update_time
+    if delta_time <= UPDATE_DURATION:
+        # unnecessary to update
+        Print.green('Unnecessary to update. Next update window after %ss' % (UPDATE_DURATION - delta_time))
+        exit_shell()
+        return
+
+    # save last update time to shell
+    shell_exec('export EC3_LAST_UPDATE_TIME=%s' % curr_update_time)
+
+    # start version check
     Print.green('Check version ...')
 
     # get local version
@@ -41,7 +95,7 @@ def launch():
     else:
         Print.green('Done. Need update! local: [%s], remote: [%s]' % (local_version, remote_version))
         Print.green('Updating ...')
-        print('cmd:git pull')
+        shell_exec('git pull')
         Print.green('Done.')
 
     # exit shell
