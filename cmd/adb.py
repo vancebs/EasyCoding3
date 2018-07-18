c70946804802b8af69ee48492fdece849e2cd346
Fan.Hu <fan.hu@t2mobile.com>
2018-07-18 17:06:32 +0800

    add command adb

---
+++ cmd/adb.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# coding=utf-8
+
+from cmd.base.Cmd import Cmd
+from script.util.Print import Print
+
+
+class adb(Cmd):
+    _INIT_WORK_DIR: bool = False
+    _RESTORE_WORK_DIR: bool = False
+
+    _HELP_MESSAGE = (
+        'adb',
+    )
+
+    def on_run(self, *params) -> bool:
+        self.shell('adb %s' % ' '.join(params))
+
+        return True
