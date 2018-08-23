617e19fae6ed84b14c41bce86689601fc3509ffd
Fan.Hu <fan.hu@t2mobile.com>
2018-08-23 15:42:59 +0800

    fix pm85

--- cfg/pm85.py
+++ cfg/pm85.py
@@ -5,7 +5,7 @@ from cfg.base.Config import Config
 
 
 class pm45(Config):
-    cfgProjectName = 'pm85'
+    cfgProjectName = 'PM85'
     cfgProjectRootDirName = 'pm85'
     cfgProjectBranch = 'o450_pm85'
     cfgProjectEnvConfig = 'choosecombo 1 %s userdebug' % cfgProjectName
