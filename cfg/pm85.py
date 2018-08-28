4cfa2bdbbbaff68443d14b6c6742d50aff752c4e
Fan.Hu <fan.hu@t2mobile.com>
2018-08-28 19:08:20 +0800

    fix pm85 choosecombo

--- cfg/pm85.py
+++ cfg/pm85.py
@@ -8,5 +8,5 @@ class pm85(Config):
     cfgProjectName = 'sdm450'  # on pm85 choosecombo project name is different with real project name
     cfgProjectRootDirName = 'pm85'
     cfgProjectBranch = 'pm85'
-    cfgProjectEnvConfig = 'choosecombo 1 %s userdebug' % 'PM85'
+    cfgProjectEnvConfig = 'choosecombo 1 %s userdebug 2' % 'PM85'
     cfgProjectUrlRepoRepository = 'quicl'
